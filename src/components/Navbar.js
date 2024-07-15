a6c33cbcbb49bc1c9f89ce7c98ea0f492fe8942d
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-14 16:47:29 -1000

    add font, icon, and make navbar elements closer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import '../componentsCss/Navbar.css';
+import icon from '../images/logos_linear-icon.svg';
 
 function Navbar() {
   return (
     <nav>
       <ul>
+        <li className='icon'><img src={icon} alt="Icon"/></li>
         <li><Link to="main" smooth={true} duration={500}>Main</Link></li>
         <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
         <li><Link to="resume" smooth={true} duration={500}>Resume</Link></li>
