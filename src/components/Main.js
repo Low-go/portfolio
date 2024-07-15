d41694fcf0ec76ac57c88dd0057eb41fcdcb2589
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-14 18:42:43 -1000

    make sections centered. Add my name large on first section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import '../componentsCss/Main.css';
 
 function Main() {
   return (
-    <Element name="main" className='section' >
-        test
+    <Element name="main" className='section'>
+      <h1>Lorran Alves Galdino</h1>
+      <a href="your-link-here" target="_blank" rel="noopener noreferrer">
+        <button>Temp</button>
+      </a>
     </Element>
   )
 }
 
-export default Main
+export default Main;
