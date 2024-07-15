0717a844963515ccb82a99de5beb730cb7e3ef08
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-14 18:55:39 -1000

    other changes to make video appear background. Not appearing yet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import '../componentsCss/Main.css';
+import videoSource from '../images/UNI.mp4';
+
 
 function Main() {
   return (
     <Element name="main" className='section'>
+      <video autoPlay loop muted className='backgroundVideo'>
+        <source src={videoSource} type='video/mp4' />
+      </video>
       <h1>Lorran Alves Galdino</h1>
       <a href="your-link-here" target="_blank" rel="noopener noreferrer">
         <button>Temp</button>
