4aa5d6e72dddec8049d9d25fdadf8a01da27d1a5
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-22 15:00:35 -1000

    Additional changes to navbar. Add google font, add media query, add hover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,6 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-scroll';
 import '../componentsCss/Navbar.css';
 
-
-
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
@@ -21,27 +19,37 @@ function Navbar() {
   };
 
   useEffect(() =>{
-    showButton()
+    showButton();
+    window.addEventListener('resize', showButton);
+    return () => window.removeEventListener('resize', showButton);
   }, []);
-
-  window.addEventListener('resize', showButton);
-
 
   return (
     <>
       <nav className='navbar'>
-        <div className='navbar-holder'>
-          <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-            LOW-GO <i class="fa-solid fa-ghost"></i>
+        <div className='navbar-container'>
+          <Link to='main' className="navbar-logo" onClick={closeMobileMenu} smooth={true} duration={500}>
+            LOW-GO <i className="fa-solid fa-ghost"></i>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
-            <i class={click ? 'fas fa-times' : "fa-solid fa-bars"}></i>
+            <i className={click ? 'fas fa-times' : "fa-solid fa-bars"}></i>
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>       
-            <li className='nav-item'><Link to="main" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Main</Link></li>
-            <li className='nav-item'><Link to="projects" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Projects</Link></li>
-            <li className='nav-item'><Link to="resume" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Resume</Link></li>
-            <li className='nav-item'><Link to="contact-me" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Contact Me</Link></li>
+            <li className='nav-item'>
+              <Link to="main" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Main</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="projects" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Projects</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="resume" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Resume</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="contact-me" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Contact Me</Link>
+            </li>
+            {/* <li className='nav-item'>
+              <Link to="contact-me" className='nav-links' onClick={closeMobileMenu} smooth={true} duration={500}>Contact Me</Link>
+            </li> */}
           </ul>
         </div>
       </nav>
