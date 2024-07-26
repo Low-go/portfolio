225ce9f1067af612c26858e3e3dfd7a5cacf21b1
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-25 21:38:57 -1000

    add video working, z index -1, behind fixed.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import '../componentsCss/Main.css';
-import videoSource from '../images/UNI.mp4';
-
+import videoSource from 'C:\\Users\\lorran\\Documents\\portfolio_site\\src\\images\\USA.mp4';
 
 function Main() {
   return (
-    <Element name="main" className='section'>
-      {/* <video autoPlay loop muted className='backgroundVideo'>
+    <Element name="main" className='main-container'>
+      <video autoPlay loop muted className='backgroundVideo'>
         <source src={videoSource} type='video/mp4' />
-      </video> */}
+      </video> 
       <h1>Lorran Alves Galdino</h1>
+      <p>A look into me</p>
       <a href="your-link-here" target="_blank" rel="noopener noreferrer">
         <button>Temp</button>
       </a>
     </Element>
-  )
+  );
 }
 
 export default Main;
