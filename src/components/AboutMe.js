96a92e133ba8714e7a7a1446981340172b10520c
Low-go <lorranalvesgaldino98@gmail.com>
2024-07-29 10:31:08 -1000

    Add portions of about me. Words, media query, and bootstrap carousel. TODO populate pictures

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import '../componentsCss/AboutMe.css';
 
 function AboutMe() {
   return (
-    <Element name="about_me" className='section'>
-        test
+    <Element name="about_me" className='about_me-section'>
+      <div className='about_me-content'>
+        <div className='about_me-text'>
+          <h2>About Me</h2>
+          <p>
+            Hey, my name is Lorran Alves Galdino and I am an International Undergraduate student pursuing a degree in Computer Science
+            at BYU-Hawaii.
+          </p>
+          <br/>
+          <p>
+          I currently work as a Systems Consultant at my Universities IT department, and also as a Web design and AI specialist 
+          where I am tasked using the DJango framework with embedding an AI assistant into my Universities library page to assist students with their research.
+          </p>
+          <br/>
+          <p>
+            Outside of work and school I really enjoy exercise, particularly working out and running, I love to skate to get around, enjoy manga, and am a huge fan of
+            poeticism within the human soul and the oulook of those who can see the world in a bitter-sweet manner.
+          </p>
+        </div>
+        <div className='about_me-carousel'>
+          <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="..." className="d-block w-100" alt="..."/>
+              </div>
+              <div className="carousel-item">
+                <img src="..." className="d-block w-100" alt="..."/>
+              </div>
+              <div className="carousel-item">
+                <img src="..." className="d-block w-100" alt="..."/>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </Element>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
