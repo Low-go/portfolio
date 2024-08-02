128d45b2806921b8c79f8732a3a900b07577983f
Low-go <lorranalvesgaldino98@gmail.com>
2024-08-02 11:41:27 -1000

    Add resume button in media queries. Dont like big screen look just yet

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,15 +10,20 @@ function Resume() {
         <div className='resume-text'>
           <h2>My Resume</h2>
           <p>Here is a list of my work experience and qualifications that I have racked up or put effort in through the years.
-            I am constantly expanding myself in roles that will allow me to grow within my career at my university, and hope to continue to 
+            I am constantly expanding myself in roles that will allow me to grow within my career at my university, and hope to continue to
             do so outside University life within the professional market.
           </p>
         </div>
         <div className='resume-thumbnail'>
-          <a href="https://drive.google.com/file/d/1Xc2Y0QfQWz8BjGaWKn65n0o1hvsdI0VN/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
-            <img src={resume} alt="resume thumbnail"/>
-          </a>
+          <div className='resume-card'>
+            <a href="https://drive.google.com/file/d/1Xc2Y0QfQWz8BjGaWKn65n0o1hvsdI0VN/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+              <img src={resume} alt="resume thumbnail"/>
+            </a>
+          </div>
         </div>
+        <a href="https://drive.google.com/file/d/1Xc2Y0QfQWz8BjGaWKn65n0o1hvsdI0VN/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="resume-button">
+          Resume
+        </a>
       </div>
     </Element>
   );
