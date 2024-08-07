99d64d5449117d17f3dedbd4533188ece89c08a7
Low-go <lorranalvesgaldino98@gmail.com>
2024-08-07 11:37:45 -1000

    Prototype contact-me page. TODO add media queries

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Element } from 'react-scroll';
-import '../componentsCss/ContactMe.css'
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from 'react-icons/fa';
+import '../componentsCss/ContactMe.css';
+import me from "../images/me.jpg"
 
 function ContactMe() {
   return (
     <Element name="contact-me" className='contactme-section'>
-        test
+      <div className="contact-container">
+        <h2 className="contact-title">Let's Connect</h2>
+        <div className="contact-card">
+          <img src={me} alt="Lorran Alves Galdino" className="profile-picture" />
+          <h3 className="name">Lorran Alves Galdino</h3>
+          <p className="job-title">Software Developer</p>
+          <div className="contact-info">
+            <div className="info-item">
+              <FaPhone className="icon" />
+              <a href="tel:18085016713">1 (808) 501-6713</a>
+            </div>
+            <div className="info-item">
+              <FaEnvelope className="icon" />
+              <a href="mailto:lorransoftwaredev@gmail.com">lorransoftwaredev@gmail.com</a>
+            </div>
+          </div>
+          <div className="social-links">
+            <a href="https://www.linkedin.com/in/lorran-alves-galdino/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="social-icon linkedin" />
+            </a>
+            <a href="https://github.com/Low-go" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="social-icon github" />
+            </a>
+          </div>
+        </div>
+      </div>
     </Element>
-  )
+  );
 }
 
-export default ContactMe
+export default ContactMe;
