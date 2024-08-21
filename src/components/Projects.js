79eba8351e27b667f06470ca361105eb58358248
Low-go <lorranalvesgaldino98@gmail.com>
2024-08-20 22:57:14 -1000

    add link to jfs youtube video demonstration

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ import '../componentsCss/Projects.css';
 function Projects() {
   const projects = [
     { title: "Fluid Simulation", image: `${process.env.PUBLIC_URL}/images/cathedral.jpg`, link: "#" },
-    { title: "JFS_Navigator", image: `${process.env.PUBLIC_URL}/images/code.jpg`, link: "#" },
+    { title: "JFS_Navigator", image: `${process.env.PUBLIC_URL}/images/code.jpg`, link: "https://www.youtube.com/watch?v=y1mFCYGkXuA" },
     { title: "Dev Blog", image: `${process.env.PUBLIC_URL}/images/dev_blog.png`, link: "https://low-go.github.io/dev_blog/" },
     { title: "Rock,Paper,Scissor Simulator", image: `${process.env.PUBLIC_URL}/images/rockPaperScissor.jpg`, link: "https://github.com/Low-go/RockxPaperxScissor-Simulator" },
     { title: "Portfolio Website", image: `${process.env.PUBLIC_URL}/images/portfolioImage.jpg`, link: "#" },
